2289fd13d760134fdf9b7e471f00531fdfac5476
baojiliu <baoji.liu@gmail.com>
2025-11-09 12:55:25 +0800

    feat: harbor和istio融合

--- apps/harbor/install.sh
+++ apps/harbor/install.sh
@@ -1,3 +1,5 @@
 helm repo add harbor https://helm.goharbor.io
 helm repo update
-helm upgrade --install harbor --version v1.18.0 harbor/harbor -f values.yaml
+helm upgrade --install harbor \
+  --version v1.18.0 harbor/harbor \
+  -f values.yaml
